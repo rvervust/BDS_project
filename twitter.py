a2db9685bde6d567abbf339020e4acdc3d0955f9
rvervust <ruben.vervust@ugent.be>
2020-04-19 14:03:04 +0200

    Cleanup and documentation

--- twitter.py
+++ twitter.py
@@ -1,19 +1,34 @@
 import tweepy
-import csv
 import pandas as pd
 import json
 
 
 class twitter_client(object):
+    """
+    This class handles the connection to the Twitter API, it provides a high level access to tweet retrieval
+    """
 
     def __init__(self, config="twitter_config.json"):
+        """
+        Initiate the client, using the api keys in a specified config file
+        :param config: json file with Twitter credentials
+        """
         with open(config, 'r') as f:
             cf = json.load(f)
             auth = tweepy.OAuthHandler(cf['consumer_key'], cf['consumer_secret'])
             auth.set_access_token(cf['access_token'], cf['access_token_secret'])
             self.api = tweepy.API(auth)
 
-    def get_tweets_by_tag(self, tag, since, until, count=100, lang="nl"):
+    def get_tweets_by_tag(self, tag, since=None, until=None, count=100, lang="nl"):
+        """
+        Retrieve tweets by a given hashtag
+        :param tag: the hashtag (including #)
+        :param since: start date in yyyy-mm-dd string format (inclusive)
+        :param until: end date in yyyy-mm-dd string format (inclusive)
+        :param count: the number of tweets to retrieve (it is specified in the API, but the tweepy library seems to ignore this and return more tweets)
+        :param lang: the tweet language to be filtered on
+        :return: DataFrame containing al tweets (a tweet per row)
+        """
         rows = []
         for tweet in tweepy.Cursor(self.api.search, q=tag, count=count,
                                    lang=lang,
@@ -26,6 +41,14 @@ class twitter_client(object):
         return pd.DataFrame(rows, columns=['tweet_id', 'created_at', 'name', 'text', 'location', 'tags'])
 
     def get_tweets_by_handle(self, handle, since=None, until=None, count=100):
+        """
+        Retrieve tweets by a given user handle
+        :param handle: The user handle (including @)
+        :param since: start date in yyyy-mm-dd string format (inclusive)
+        :param until: end date in yyyy-mm-dd string format (inclusive)
+        :param count: the number of tweets to retrieve (it is specified in the API, but the tweepy library seems to ignore this and return more tweets)
+        :return: DataFrame containing al tweets (a tweet per row)
+        """
         rows = []
         for tweet in self.api.user_timeline(screen_name=handle, count=count, include_rts=False, tweet_mode="extended",
                                             since=since,
